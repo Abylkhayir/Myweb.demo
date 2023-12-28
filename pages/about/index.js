f00c4cd69573fd2d061e82fec5b9d4ea54e7c5da
Abylkhayir <88201428+Abylkhayir@users.noreply.github.com>
2023-12-28 20:31:37 +0600

    Add new files
    Add new files

---
+++ pages/about/index.js
@@ -0,0 +1,170 @@
+import React, { useState } from "react";
+
+// icons
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaReact,
+} from "react-icons/fa";
+
+import {
+  SiNextdotjs,
+  SiFramer,
+  SiJavascript,
+} from "react-icons/si";
+
+//  about data
+export const aboutData = [
+  {
+    title: "skills",
+    info: [
+      {
+        title: "Web frameworks",
+        icons: [
+          <FaHtml5 key="html5" />,
+          <FaCss3 key="css3" />,
+          <FaJs key="js" />,
+          <FaReact key="react" />,
+          <SiFramer key="framer" />,
+          <SiNextdotjs key="nextjs" />,
+        ],
+      },
+      {
+        title: "Programming Languages",
+        icons: [
+          <SiJavascript key="javascript" />,
+        ],
+      },
+      {
+        title: "LLMS experience",
+        stage: [
+          "GPT-3.5",
+          "GPT-4",
+        ],
+      },
+    ],
+  },
+  {
+    title: "Education",
+    info: [
+      {
+        title: "Kazguu University",
+        stage: "Bachelor of Finance",
+      },
+      {
+        title: "Udemy",
+        stage: "Course JS+React+Redux",
+      },
+    ],
+  },
+  {
+    title: "experience",
+    info: [
+      {
+        title: "Freelance",
+        stage: "In current",
+      },
+    ],
+  },
+];
+
+// components
+import Avatar from "../../components/Avatar";
+import Circles from "../../components/Circles";
+
+// framer motion
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
+
+// counter
+import CountUp from "react-countup";
+
+const About = () => {
+  const [index, setIndex] = useState(0);
+  console.log(index);
+  return (
+    <div className="h-full bg-primary/30 py-32 text-center xl:text-left">
+      <motion.div
+        variants={fadeIn("right", 0.2)}
+        initial="hidden"
+        animate="show"
+        exit="hidden"
+        className="hidden xl:flex absolute bottom-0 -left-[370px]"
+      >
+        <Avatar />
+      </motion.div>
+      <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
+        {/* text */}
+        <div className="flex-1 flex flex-col justify-center">
+          <motion.h2
+            variants={fadeIn("right", 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h2"
+          >
+            <span className="text-accent">Frontend SDE</span>
+          </motion.h2>
+          <motion.p
+            variants={fadeIn("right", 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0"
+          >
+            For a long time I studied independently, through the Internet and free resources to master the basics. Subsequently, I also studied with the participation of a mentor and took an additional course on Udemy and RS-School
+          </motion.p>
+        </div>
+        {/* info */}
+        <motion.div
+          variants={fadeIn("left", 0.4)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          className="flex flex-col w-full xl:max-w-[48%] h-[480px]"
+        >
+          <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
+            {aboutData.map((item, itemIndex) => {
+              return (
+                <div
+                  key={itemIndex}
+                  className={`${
+                    index === itemIndex &&
+                    "text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300"
+                  }  cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
+                  onClick={() => setIndex(itemIndex)}
+                >
+                  {item.title}
+                </div>
+              );
+            })}
+          </div>
+          <div className="py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start">
+            {aboutData[index].info.map((item, itemIndex) => {
+              return (
+                <div
+                  key={itemIndex}
+                  className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60"
+                >
+                  {/* title */}
+                  <div className="font-light mb-2 md:mb-0">{item.title}</div>
+                  <div className="hidden md:flex">-</div>
+                  <div key={itemIndex}>{item.stage}</div>
+                  <div className="flex gap-x-4">
+                    {/* icons */}
+                    {item.icons?.map((icon, iconIndex) => {
+                      return <div key={iconIndex} className="text-2xl text-white">{icon}</div>;
+                    })}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
